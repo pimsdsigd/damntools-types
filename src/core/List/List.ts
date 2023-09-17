45a8b59b1fac5f22e3991781da33f42d41bedffb
Florian Castera <florian.castera@gmail.com>
2023-09-17 18:54:38 +0200

    fixed list signature

--- src/core/List/List.ts
+++ src/core/List/List.ts
@@ -33,7 +33,11 @@ export interface List<T> {
 
   includes(item: T): boolean
 
-  forEach(action: PeekFunction<T>): this
+  forEach(action: (value: T) => void): this
+
+  forEach(action: (value: T, index: number) => void): this
+
+  forEach(action: (value: T, index: number, array: Array<T>) => void): this
 
   getInner(): Array<T>
 
@@ -53,13 +57,21 @@ export interface List<T> {
 
   concat(...items: ConcatArgType<T>): this
 
-  insert(start: number, items?: AbstractedArray<T>): this
+  insert(start: number): this
 
-  replace(start: number, items?: AbstractedArray<T>): this
+  insert(start: number, items: AbstractedArray<T>): this
 
-  replaceFrom(start: number, items?: AbstractedArray<T>): this
+  replace(start: number): this
 
-  sub(start: number, end?: number): this
+  replace(start: number, items: AbstractedArray<T>): this
+
+  replaceFrom(start: number): this
+
+  replaceFrom(start: number, items: AbstractedArray<T>): this
+
+  sub(start: number): this
+
+  sub(start: number, end: number): this
 
   toString(): string
 }
