78bebaded18673be5298908cf87d9f489f673719
Florian Castera <florian.castera@gmail.com>
2024-11-20 20:03:08 +0100

    added referenceAccessor implementation

--- src/utils/ReferenceAccessor/ReferenceNumberAccessor.ts
+++ src/utils/ReferenceAccessor/ReferenceNumberAccessor.ts
@@ -4,4 +4,34 @@ export class ReferenceNumberAccessor<T> extends ReferenceAccessor<T, number> {
   constructor(field: keyof T | string) {
     super(field, (a, b) => b === a)
   }
+
+  public incrementAndGet(obj: T): number {
+    const value = this.get(obj) + 1
+    this.set(obj, value)
+    return value
+  }
+
+  public decrementAndGet(obj: T): number {
+    const value = this.get(obj) - 1
+    this.set(obj, value)
+    return value
+  }
+
+  public getAndIncrement(obj: T): number {
+    const value = this.get(obj)
+    this.set(obj, value + 1)
+    return value
+  }
+
+  public getAndDecrement(obj: T): number {
+    const value = this.get(obj)
+    this.set(obj, value - 1)
+    return value
+  }
+
+  public addAndGet(obj: T, add: number): number {
+    const value = this.get(obj)
+    this.set(obj, value + add)
+    return value
+  }
 }
