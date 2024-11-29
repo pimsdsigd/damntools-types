52eb9c5415afb4a44ca83c6ab88055b095037970
Florian Castera <florian.castera@gmail.com>
2024-11-29 21:48:40 +0100

    adde dict.map

--- src/utils/Promise/Promises.test.ts
+++ src/utils/Promise/Promises.test.ts
@@ -1,8 +1,6 @@
-import {Promises} from "./Promises"
 import "./index"
 import "../../index.types"
 
-const array = [1, 2, 6, 3, 6, 8]
 
 describe("Promise", () => {
   describe("thenDo()", () => {
@@ -75,97 +73,12 @@ describe("Promise", () => {
     it("with auto start", done => {
       Promise.resolve("text")
         .zipWith(Promise.resolve("prom"))
-        .zipWith(v => Promise.resolve( v[1].toUpperCase()))
+        .zipWith(v => Promise.resolve(v[1].toUpperCase()))
         .then(v => {
           console.log("res=", v)
           expect(v[0][0]).toBe("text")
           expect(v[0][1]).toBe("prom")
           expect(v[1]).toBe("PROM")
-          done()
-        })
-    })
-  })
-})
-
-describe("Promises", () => {
-  describe("sequence()", () => {
-    it("correct", done => {
-      const time = new Date().getTime()
-      const array = [
-        () => new Promise(resolve => {
-          console.log("constructing 1", new Date().getTime() - time)
-          setTimeout(() => {
-            console.log("timeout 1", new Date().getTime() - time)
-            resolve(new Date().getTime() - time);
-          }, 100);
-        }),
-        () => new Promise(resolve => {
-          console.log("constructing 2", new Date().getTime() - time)
-          setTimeout(() => {
-            console.log("timeout 2", new Date().getTime() - time)
-            resolve(new Date().getTime() - time);
-          }, 100);
-        }),
-        () => new Promise(resolve => {
-          console.log("constructing 3", new Date().getTime() - time)
-          setTimeout(() => {
-            console.log("timeout 3", new Date().getTime() - time)
-            resolve(new Date().getTime() - time);
-          }, 100);
-        }),
-      ]
-      Promises.sequence(array)
-        .then(res => {
-          expect(res).toBeGreaterThan(300)
-          done()
-        })
-
-    })
-  })
-
-  describe("allSuccess()", () => {
-    it("all success returns only data", done => {
-      const promises = array.map(v => {
-        return Promise.resolve(v)
-      })
-      Promises.allSuccess(promises)
-        .then(res => {
-          expect(res.getInner()).toBe(array)
-          done()
-        })
-        .catch(err => {
-          console.log(err)
-          expect(true).toBeTruthy()
-          done()
-        })
-    })
-    it("one ko throws 1 error", done => {
-      const promises = array.map(v => {
-        if (v === 3) return Promise.reject("error" + v)
-        return Promise.resolve(v)
-      })
-      Promises.allSuccess(promises)
-        .then(() => {
-          expect(false).toBeTruthy()
-        })
-        .catch(err => {
-          expect(err.size()).toBe(1)
-          expect(err.first().get()).toBe("error3")
-          done()
-        })
-    })
-    it("two ko throws 2 error", done => {
-      const promises = array.map(v => {
-        if (v === 6) return Promise.reject("error" + v)
-        return Promise.resolve(v)
-      })
-      Promises.allSuccess(promises)
-        .then(() => {
-          expect(false).toBeTruthy()
-        })
-        .catch(err => {
-          expect(err.size()).toBe(2)
-          expect(err.first().get()).toBe("error6")
           done()
         })
     })
