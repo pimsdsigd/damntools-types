58598095eee590fc723a5f5c518746f32add6e35
Florian Castera <florian.castera@gmail.com>
2025-01-30 19:51:25 +0100

    adde type utils method

--- src/utils/TypeUtils.ts
+++ src/utils/TypeUtils.ts
@@ -4,6 +4,7 @@ export class TypeUtils {
   static getClassName<T>(theClass: ClassType<T>): string {
     return theClass.name
   }
+
   static instanceOf<T>(value: T, theClass: AbstractType<T>): boolean {
     return (
       (value && value instanceof (theClass as ClassType<T>)) ||
@@ -11,6 +12,10 @@ export class TypeUtils {
       (theClass === Boolean && typeof value === "boolean") ||
       (theClass === String && typeof value === "string")
     )
+  }
+
+  static isClass<T>(theClass: AbstractType<T>): theClass is ClassType<T> {
+    return (theClass && !!theClass.new)
   }
 
   static isRichType(obj: any): boolean {
