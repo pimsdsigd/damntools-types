06cf7c45e0158e27967be60f16b8ddd82b7494b9
Florian Castera <florian.castera@gmail.com>
2026-01-08 12:23:59 +0100

    fixed atomic reference

--- src/utils/Atomic/AtomicReference.ts
+++ src/utils/Atomic/AtomicReference.ts
@@ -3,13 +3,45 @@ import {Optional} from "../../optional"
 
 export class AtomicReference<T> {
   protected value: Optionable<T>
+  private readonly defaultValue?: Optionable<T>
 
   constructor(initialValue?: T) {
     this.value = Optional.nullable(initialValue)
+    this.defaultValue = this.value
   }
 
-  set(value?: T): void {
+  set(value: T): void {
     this.value = Optional.nullable(value)
+  }
+
+  clear(): void {
+    this.value = Optional.empty()
+  }
+
+  clearAndGet(): T {
+    this.value = Optional.empty()
+    return this.value.orElseUndefined()
+  }
+
+  getAndClear(): T {
+    const get = this.value.orElseUndefined()
+    this.value = Optional.empty()
+    return get
+  }
+
+  reset(): void {
+    this.value = this.defaultValue
+  }
+
+  resetAndGet(): T {
+    this.value = this.defaultValue
+    return this.value.orElseUndefined()
+  }
+
+  getAndReset(): T {
+    const get = this.value.orElseUndefined()
+    this.value = this.defaultValue
+    return get
   }
 
   get(): T {
