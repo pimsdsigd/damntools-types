0e0e76e57cdfeb05518aa960143a94738af8ae1d
Florian Castera <florian.castera@gmail.com>
2026-01-20 21:31:39 +0100

    added basic queue

--- src/list/BasicQueue/BasicQueue.ts
+++ src/list/BasicQueue/BasicQueue.ts
@@ -1,9 +1,15 @@
 import {List, Queue} from "../../core"
 import {QueueEmptyError} from "../../exceptions"
+import {ArrayList} from "../ArrayList"
 
 export class BasicQueue<T> implements Queue<T> {
   private readonly maxSize: number
   private readonly queue: List<T>
+
+  constructor(maxSize?: number) {
+    this.maxSize = Math.min(maxSize || Number.MAX_VALUE, Number.MAX_VALUE)
+    this.queue = new ArrayList()
+  }
 
   hasElements(): boolean {
     return this.queue.hasElements()
