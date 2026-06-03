924799eb4918a27713d37bfa0858095cde4ec161
Florian Castera <florian.castera@gmail.com>
2026-06-03 16:33:21 +0200

    added streams.concat

--- src/utils/Streams/Streams.ts
+++ src/utils/Streams/Streams.ts
@@ -1,9 +1,16 @@
-import {abstractArrayToArray, AbstractedArray, Stream} from "../../core"
+import {abstractArrayToArray, AbstractedArray, notDefined, Stream} from "../../core"
 import {ListStream} from "../../stream"
 
 export abstract class Streams {
   static from<T>(array: AbstractedArray<T>): Stream<T> {
     const arr = abstractArrayToArray(array)
     return new ListStream(arr)
+  }
+
+  static concat<T>(...streams: Stream<T>[]): Stream<T> {
+    if (notDefined(streams)) streams = []
+    return streams.reduce((o, c) => {
+      return o.concat(c)
+    }, Streams.from<T>([]))
   }
 }
